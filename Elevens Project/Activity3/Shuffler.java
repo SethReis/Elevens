456efe197240e0889a25e2698a27529f83a90d73
SethReis <795091@seq.org>
2017-01-20 08:52:00 -0800

    Add files via upload

--- Elevens Project/Activity3/Shuffler.java
+++ Elevens Project/Activity3/Shuffler.java
@@ -50,7 +50,17 @@ public class Shuffler {
 	 * @param values is an array of integers simulating cards to be shuffled.
 	 */
 	public static void perfectShuffle(int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+		int[] shuffled = new int[int[] values]];
+		k = 0;
+		for (int i = 0; i < values.length/2; i++){
+		    shuffled[k] = values[i];
+		    k = k + 2;
+		}
+		
+		for (int j = values.length/2; j < values.length; j++){
+		    sheffled[k] = values[j];
+		    k = k + 2;
+	    }
 	}
 
 	/**
